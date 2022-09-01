703848ab06435d88837ef5ce0c564610b13c6926
Ryan Enns <89048358+letsbefriendzz@users.noreply.github.com>
2022-09-01 09:16:54 -0400

    sanctum

--- lcbo-rank-backend/routes/api.php
+++ lcbo-rank-backend/routes/api.php
@@ -1,12 +1,14 @@
 <?php
 
-use App\Http\Controllers\BeerController;
-use App\Http\Controllers\CoolerController;
-use App\Http\Controllers\SpiritsController;
-use App\Http\Controllers\WineController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('alcohol')->group(function () {
-    Route::get('/efficient', 'App\Http\Controllers\AlcoholController@getEfficient');
-    Route::get('/', 'App\Http\Controllers\AlcoholController@getDefault');
+    Route::get('/efficient', 'App\Http\Controllers\AlcoholController@getEfficient')->middleware(['auth:sanctum']);
+    Route::get('/', 'App\Http\Controllers\AlcoholController@getDefault')->middleware(['auth:sanctum']);
+});
+
+Route::post('/tokens/create', function (Request $request) { // TODO create controller
+    $token = $request->user()->createToken($request->token_name);
+    return ['token' => $token->plainTextToken];
 });
